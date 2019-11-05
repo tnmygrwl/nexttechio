e235dbad4bf3eb155d82aa6f9e4b3672c7dddd7f
prashant9912 <prashantg9912@gmail.com>
2019-11-05 18:56:29 +0530

    added login page!

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,62 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
+import { FiArrowLeft } from "react-icons/fi"; //icons
+import './styles/login.scss';
+import Fade from 'react-reveal/Fade'; // Importing Zoom effect
+import { NavLink } from 'react-router-dom'
 
 
-export default class Login extends Component{
-    
-    render(){
-        return <div>This is login page</div>
-    }
+import {
+    AwesomeButton,
+  } from 'react-awesome-button';
+
+//   import  "react-awesome-button/src/styles/styles.scss";
+import 'react-awesome-button/dist/themes/theme-blue.css';
+
+
+
+
+export default class Login extends Component {
+
+    render() {
+        return <div className='cont'>
+            <NavLink to="/">    <FiArrowLeft size={30} /></NavLink>
+            <Fade className="top">
+
+                {/* <Container className="container">
+
+                    <Row>
+                       
+                        <Col sm="12" md={{ size: 6, offset: 4 }}>.col-sm-12 .col-md-6 .offset-md-3</Col>
+                    </Row>
+
+                </Container> */}
+                <div className="container">
+                    <div className='logincont'>
+                        <b>LOGIN</b>
+                        <div className='space'></div>
+
+                            <div className='space'></div>
+                        <label for="inp" class="inp">
+                            <input type="text" id="inp" placeholder="&nbsp;"/>
+                                <span class="label">User Name</span>
+                                <span class="border"></span>
+                            </label>
+                            <div className='space'></div>
+                            <label for="inp" class="inp">
+                            <input type="password" id="inp" placeholder="&nbsp;"/>
+                                <span class="label">Password</span>
+                                <span class="border"></span>
+                            </label>
+                            <br></br><br></br>
+                            
+                            <AwesomeButton size="large" type="primary">Go > </AwesomeButton>
+
+
+                        </div>
+
+                    </div>
+            </Fade>
+
+        </div>
+            }
 }
